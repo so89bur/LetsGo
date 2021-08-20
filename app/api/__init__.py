8dc959bf5cf8d8666b4a8fefbd09ed8c966cf5d3
Ivan Sovin <sovin.is@electromicro.ru>
2021-08-21 02:11:50 +0300

    Init server & db model

---
+++ app/api/__init__.py
@@ -0,0 +1,24 @@
+import json
+
+from flask import request
+from functools import wraps
+
+from app import db
+from app.models import User
+
+
+def support(func):
+  def func_wrapper(*args, **kwargs):
+    try:
+      for i in range(ATTEMPTS_NUMBER):
+        try:
+          return func(*args, **kwargs)
+        except Exception:
+          continue
+        break
+    except Exception as e:
+      db.session.rollback()
+      return json.dumps({'success': False})
+    finally:
+      db and db.close()
+    return func_wrapper
